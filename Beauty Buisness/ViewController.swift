74e0eb0988a3a79d608e4e69a16211a0a42d5d07
Vasiliy Andreyev <vaasilly@yandex.ru>
2022-03-26 22:41:01 +0300

    Initial Commit

---
+++ Beauty Buisness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beauty Buisness
+//
+//  Created by Vasiliy Andreyev on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
